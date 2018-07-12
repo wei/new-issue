1321f52dc0dd3d4381477ed3dfd9729e8c3c5024
Wei He <git@weispot.com>
2018-07-11 20:32:05 -0400

    :arrow_up: Upgrade ESLint

--- preact.config.js
+++ preact.config.js
@@ -1,13 +1,13 @@
 import CopyWebpackPlugin from 'copy-webpack-plugin'
 
 export default (config, env, helpers) => {
-  config.plugins.push( new CopyWebpackPlugin([
+  config.plugins.push(new CopyWebpackPlugin([
     {
       context: `${__dirname}`,
       from: `public`,
       force: true,
-    }
-  ]));
+    },
+  ]))
 
   delete config.entry.polyfills
   config.output.filename = '[name].js'
